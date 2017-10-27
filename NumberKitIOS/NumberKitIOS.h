43d6ee3a1f30e5cb118640f1e3d85f498b55117d
Clint Hanson <Anthony.C.Hanson.jr@saic.com>
2017-10-27 12:48:43 -0500

    Added podspec

---
+++ NumberKitIOS/NumberKitIOS.h
@@ -0,0 +1,18 @@
+//
+//  NumberKitIOS.h
+//  NumberKitIOS
+//
+//  Created by Hanson, Anthony C. Jr on 10/27/17.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NumberKitIOS.
+FOUNDATION_EXPORT double NumberKitIOSVersionNumber;
+
+//! Project version string for NumberKitIOS.
+FOUNDATION_EXPORT const unsigned char NumberKitIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NumberKitIOS/PublicHeader.h>
+
+
